bef4402a523992d2a629d2baff9f408fe42fd219
eddiemofokeng <eddie.mofokekeng@dentsuaegis.com>
2021-02-25 23:25:47 +0200

    creating app insights module for monitoring apim events

---
+++ modules/app-insights/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_application_insights" "app-insights" {
+  name                = "${var.base_name}-appinsights"
+  location            = var.region
+  resource_group_name = var.rg_name
+  application_type    = var.app_type
+
+    tags = merge({
+    Environment = var.environment
+    Product     = var.base_name
+  }, var.tags)
+}
