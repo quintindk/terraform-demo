3b5a955bd190cfa7a108e109f359158f4ad369f2
prinsmike <mike.prinsloo@gmail.com>
2021-02-28 20:24:35 +0200

    feat: Lay out the network.

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "azurerm" {
 
 # Local Vars
 locals {
-  base_name        = "demo"
+  base_name = "demo"
 }
 
 resource "azurerm_security_center_subscription_pricing" "security_centre" {
@@ -33,28 +33,28 @@ resource "azurerm_security_center_subscription_pricing" "security_centre" {
 }
 
 module "rg_shared" {
-    source = "./modules/rg"
-    
-    region      = var.region
-    base_name   = "shared"
-    environment = var.environment
-    tags        = var.tags
+  source = "./modules/rg"
+
+  region      = var.region
+  base_name   = "shared"
+  environment = var.environment
+  tags        = var.tags
 }
 
 module "rg_network" {
-    source = "./modules/rg"
-    
-    region      = var.region
-    base_name   = "network"
-    environment = var.environment
-    tags        = var.tags
+  source = "./modules/rg"
+
+  region      = var.region
+  base_name   = "network"
+  environment = var.environment
+  tags        = var.tags
 }
 
 module "vnet" {
   source = "./modules/vnet/vnet"
 
-  region = var.region
-  base_name = local.base_name
+  region      = var.region
+  base_name   = local.base_name
   environment = var.environment
 
   rg_name = module.rg_network.name
@@ -64,4 +64,33 @@ module "vnet" {
   tags = var.tags
 }
 
+module "nsg" {
+  source = "./modules/nsg"
 
+  region      = var.region
+  environment = var.environment
+  base_name   = local.base_name
+  rg_name     = module.rg_network.name
+
+  security_rules = [{
+    name                         = "${module.vnet.name}-nsg"
+    priority                     = 100
+    direction                    = "inbound"
+    access                       = "Deny"
+    protocol                     = "Tcp"
+    source_port_ranges           = ["*"]
+    destination_port_ranges      = ["22", "80", "443"]
+    source_address_prefixes      = ["*"]
+    destination_address_prefixes = ["*"]
+  }]
+}
+
+module "subnet" {
+  source = "./modules/vnet/subnet_nsg"
+
+  rg_name          = module.rg_network.name
+  vnet_name        = module.vnet.name
+  address_prefixes = ["10.0.1.0/24"]
+
+  nsg_id = module.nsg.id
+}
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
   description = "The region where resources will be deployed."
-  type = string
+  type        = string
 }
 
 variable "environment" {
   description = "The environment where resources are deployed."
-  type = string
+  type        = string
 }
 
 variable "tags" {
   description = "Tags to associate with Azure resource groups."
-  type = map(string)
+  type        = map(string)
   default {}
 }
